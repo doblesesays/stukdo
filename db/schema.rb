8d8a96dd92c0e0575a59f59663dea23efcbf1738
Genessis Jimenez <Genesis.jz.93@gmail.com>
2016-09-01 22:19:43 -0400

    Se agrego la propiedad state a las tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160901191654) do
+ActiveRecord::Schema.define(version: 20160901200441) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.integer  "user_id"
+    t.string   "state",      default: "to_do"
   end
 
   create_table "users", force: :cascade do |t|
