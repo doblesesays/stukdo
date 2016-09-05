aa38364d5b9fa85b4a3eff173d93d7d2ec18b734
Genessis Jimenez <Genesis.jz.93@gmail.com>
2016-09-05 15:47:04 -0400

    Agrege un nuevo filtro para resize las img (Implementación mia)

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,8 @@ class Task < ActiveRecord::Base
 	belongs_to :user
 	auto_html_for :content do
 		html_escape
-		image
+		# image
+		sized_image
 		youtube(width: '100%', height: 250)
 		link target: '_blank', rel: 'nofollow'
 		simple_format
